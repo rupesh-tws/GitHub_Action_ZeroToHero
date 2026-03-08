70b58a47d0d80e2f425e55ee7475210364686e0c
Rupesh Khurpe <rupesh.k@corp.cumulus>
2026-03-08 12:05:20 +0530

    make change in workflow

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from flask  import Flas
+from flask  import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/health')
+def health():
+    return 'server is up and running'
